fab6dd9502b10855067b536187f6217eb85d9578
huahhuah <149953060+huahhuah@users.noreply.github.com>
2025-03-15 15:43:43 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-todo/',
+  // base: '/vue-todo/', //for vercel depoly
   plugins: [vue()],
   resolve: {
     alias: {
